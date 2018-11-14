86de5ddbd7ff722b8a894e5ac07dd56ab0993c6a
Volkan Gürbüz <volkangurbuz16@gmail.com>
2018-11-14 11:14:04 +0300

    Update Trend.java

--- app/src/main/java/com/volkangurbuz/codechallenge/Model/Trend.java
+++ app/src/main/java/com/volkangurbuz/codechallenge/Model/Trend.java
@@ -7,52 +7,97 @@ import java.io.Serializable;
 
 public class Trend implements Serializable {
 
-    @SerializedName("username")
-    @Expose
-    private String name;
+        @SerializedName("author")
+        @Expose
+        private String author;
+        @SerializedName("name")
+        @Expose
+        private String name;
+        @SerializedName("url")
+        @Expose
+        private String url;
+        @SerializedName("description")
+        @Expose
+        private String description;
+        @SerializedName("language")
+        @Expose
+        private String language;
 
-    @SerializedName("name")
-    @Expose
-    private String nameDetail;
+        @SerializedName("stars")
+        @Expose
+        private int stars;
+        @SerializedName("forks")
+        @Expose
 
-    @SerializedName("url")
-    @Expose
-    private String url;
+        private int forks;
+        @SerializedName("currentPeriodStars")
+        @Expose
+        private Integer currentPeriodStars;
 
-    @SerializedName("repo")
-    @Expose
-    private RepoDetails reponame;
+        public String getAuthor() {
+            return author;
+        }
 
+        public void setAuthor(String author) {
+            this.author = author;
+        }
 
-    public RepoDetails getReponame() {
-        return reponame;
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getLanguage() {
+            return language;
+        }
+
+        public void setLanguage(String language) {
+            this.language = language;
+        }
+
+        public int getStars() {
+            return stars;
+        }
+
+        public void setStars(int stars) {
+            this.stars = stars;
+        }
+
+        public int getForks() {
+            return forks;
+        }
+
+        public void setForks(int forks) {
+            this.forks = forks;
+        }
+
+        public Integer getCurrentPeriodStars() {
+            return currentPeriodStars;
+        }
+
+        public void setCurrentPeriodStars(Integer currentPeriodStars) {
+            this.currentPeriodStars = currentPeriodStars;
+        }
+
     }
 
-    public void setReponame(RepoDetails reponame) {
-        this.reponame = reponame;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNameDetail() {
-        return nameDetail;
-    }
-
-    public void setNameDetail(String nameDetail) {
-        this.nameDetail = nameDetail;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-}
